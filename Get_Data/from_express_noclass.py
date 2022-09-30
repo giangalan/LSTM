5aecbc3669b92bec50f67fbef5c654ee92a8299d
Phan Minh Giang <103631986+giangalan@users.noreply.github.com>
2022-09-30 17:34:37 +0700

    Add files via upload

---
+++ Get_Data/from_express_noclass.py
@@ -0,0 +1,49 @@
+import re
+from builtins import type
+
+from bs4 import BeautifulSoup
+
+import requests
+from prometheus_client.decorator import getfullargspec
+
+
+def get_link(topic_link):
+    topicurl = requests.get(topic_link)
+    topic = BeautifulSoup(topicurl.content)
+    links = topic.findAll('article', attrs={'class': 'item-news item-news-common '})
+    # for link in links:
+
+# print (link)
+
+
+def get_website(link):
+    url = requests.get(link)
+    news = BeautifulSoup(url.content)
+    return news
+
+
+def get_data(news) -> dict:
+    get_title = news.find('h1').text
+    get_description = news.find('p', attrs={'class': 'description'}).text
+    get_detail = news.find('article', attrs={'class': 'fck_detail'})
+    get_text = get_detail.findAll('p', attrs={'class': 'Normal'})
+    data = ''
+    data += 'title:' + get_title + '\n'
+    data += 'description:' + get_description + '\n'
+    data += 'content:'
+    for item in get_text:
+        data += item.text + '\n'
+    return data
+
+
+def save_data(data, filepath):
+    file = open(filepath, 'a')
+    file.write(data)
+    file.close()
+
+
+filepath = '/NLP/First_model_of_mine/Get_Data/dataset.txt'
+link = 'https://vnexpress.net/nga-dieu-tra-khung-bo-quoc-te-vu-ro-ri-nord-stream-4517046.html'
+news = get_website(link)
+data = get_data(news)
+save_data(data, filepath)
---
+++ Get_Data/from_vn_express.py
@@ -0,0 +1,43 @@
+import re
+from bs4 import BeautifulSoup
+
+import requests
+from prometheus_client.decorator import getfullargspec
+
+
+class DataFromNews:
+    def __init__(self, filepath, link):
+        self.filepath = filepath
+        self.link = link
+
+    def get_website(self):
+        url = requests.get(self.link)
+        news = BeautifulSoup(url.content)
+        return news
+
+    def get_data(self):
+        news = self.get_website(self.link)
+        get_title = news.find('h1').text
+        get_description = news.find('p', attrs={'class': 'description'}).text
+        get_detail = news.find('article', attrs={'class': 'fck_detail'})
+        get_text = get_detail.findAll('p', attrs={'class': 'Normal'})
+        data = {}
+        data['titlte'] = get_title
+        data['description'] = get_description
+        text = []
+        for item in get_text:
+            text.append(item.text)
+        data['content'] = text
+        return data
+
+    def save_data(self):
+        data = self.get_data(self.link)
+        file = open(self.filepath, 'a')
+        file.write(data)
+        file.close()
+
+
+if __name__ == '__main__':
+    links = 'https://vnexpress.net/nga-dieu-tra-khung-bo-quoc-te-vu-ro-ri-nord-stream-4517046.html'
+    DataFromNews('/NLP/First_model_of_mine/Get_Data/dataset.txt', links)
+    DataFromNews.save_data()
