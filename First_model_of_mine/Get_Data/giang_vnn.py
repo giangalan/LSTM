40949934123bb5b20e1666a176120765fb516baa
Phan Minh Giang <103631986+giangalan@users.noreply.github.com>
2022-10-04 07:43:02 +0700

    Add files via upload

--- First_model_of_mine/Get_Data/giang_vnn.py
+++ First_model_of_mine/Get_Data/giang_vnn.py
@@ -1,74 +1,96 @@
+import datetime
 import re
 import concurrent
+import time
+
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 from multiprocessing import Process
 import hashlib
 
-dict = {'title': [], 'description': [], 'topic': []}
+dict = {'inde': [], 'titl': [], 'desc': [], 'cont': [], 'tmup': [], 'tmcr': [], 'topi': []}
 failed_links = []
+failed_titles = []
 
 
-def crawl(links, tl):
+def crawl(links, topi):
+    domain = 'https://vietnamnet.vn'
+    a = 0
     for link in links:
-        news = requests.get(link)
-        if news.status_code == 200:
+        try:
+            news = requests.get(link)
             soup = BeautifulSoup(news.content, "html.parser")
-            try:
-                title = soup.find("h1", class_="title-detail").text
-                descriptiont = soup.find("p", class_="description").text
-                description = re.sub(r';', '', descriptiont)
-                # body = soup.find("article", class_="fck_detail")
-                # pchil = body.findChildren("p", class_="Normal", recursive=False)
-                # text = ''
-                # for child in pchil:
-                #     text += child.text
-                dict['title'].append(title)
-                dict['description'].append(description)
-                # data['content'] = text
-                dict['topic'].append(tl)
-                dict['URL'] = hashlib.md5(link[22:].encode()).hexdigest()
-            except:
-                failed_links.append(link)
+        except:
+            news = requests.get(domain + link)
+            soup = BeautifulSoup(news.content, 'html.parser')
+        try:
+            a += 1
+            print('processing', a)
+            time_up = str(soup.find('div', class_='breadcrumb-box__time').find('span').text)
+            tmup = time_up[30:39] + ' ' + time_up[42:47]
+            title = soup.find('h1').text
+            description = str(soup.find('div', class_='newFeature__main-textBold').text).strip()
+            # description = re.sub(r';', '', descriptiont)
+            body = soup.find("div", class_="maincontent").find('div')
+            pchil = body.findChildren("p", recursive=False)
+            text = ''
+            for child in pchil:
+                text += ' ' + child.text
+            dict['tmcr'].append(str(datetime.datetime.now().strftime('%H:%M %d-%m-%Y')))
+            dict['tmup'].append(tmup)
+            dict['titl'].append(title)
+            dict['desc'].append(description)
+            dict['cont'].append(text)
+            dict['topi'].append(topi)
+            dict['inde'].append(hashlib.md5(link[22:].encode()).hexdigest())
+        except:
+            failed_links.append(link)
 
 
-def cr_pm(tl, i):
-    extend = ''
-    if i >= 2:
-        extend = '-p' + str(i)
-    url = 'https://vnexpress.net/' + tl + extend
+def cr_pm(topi, i):
+    extend = '-page' + str(i)
+    if topi == 1:
+        tl = 'the-gioi'
+    elif topi == 2:
+        tl = 'the-thao'
+    elif topi == 3:
+        tl = 'phap-luat'
+    print('Crawling ' + tl + extend)
+    url = 'https://vietnamnet.vn/' + tl + extend
     response = requests.get(url)
     if response.status_code == 200:
         sour_parent = BeautifulSoup(response.content, "html.parser")
-        titles = sour_parent.findAll('h2', class_='title-news')
-        if titles:
-            links = [link.find('a').attrs["href"] for link in titles]
-            crawl(links, tl)
-        else:
-            titles = sour_parent.findAll('h3', class_='title-news')
-            links = [link.find('a').attrs["href"] for link in titles]
-            crawl(links, tl)
+        vtitles = sour_parent.findAll('h3', class_='video__related-title vnn-title')
+        if i == 0:
+            titles = sour_parent.findAll('h3', class_='vnn-title')
+        elif i >= 1:
+            titles = sour_parent.findAll('h3', class_='feature-box__content--title vnn-title')
+        links = [link.find('a').attrs["href"] for link in titles if link not in vtitles]
+        crawl(links, topi)
 
 
-def mul_pro(tl):
-    for i in range(1, 21):
-        cr_pm(tl, i)
+def mul_pro(topi):
+    for i in range(0, 1001):
+        cr_pm(topi, i)
 
 
 if __name__ == '__main__':
-    topic_lists = ['the-gioi', 'the-thao', 'phap-luat']
-    processes = []
-    for topic in topic_lists:
-        mul_pro(topic)
-        p = Process(target=mul_pro, args=[topic])
-        p.start()
-        processes.append(p)
-    for process in processes:
-        process.join()
+    for t in range(1,4):
+        mul_pro(t)
+# topic_lists = ['the-gioi', 'the-thao', 'phap-luat']
+# processes = []
+# for topic in topic_lists:
+#     mul_pro(topic)
+#     p = Process(target=mul_pro, args=[topic])
+#     p.start()
+#     processes.append(p)
+# for process in processes:
+#     process.join()
+# crawl(['https://vietnamnet.vn/khong-quan-iran-duoc-dau-tu-nang-cao-suc-manh-2063822.html'], 1)
 
-    # mul_pro('thoi-su')
+# mul_pro('thoi-su')
 # with Pool(6) as pool:
 #     pool.map(cr_pm, range(2, 18), chunksize=3)
 df = pd.DataFrame(dict)
-df.to_csv('/home/code/NLP/First_model_of_mine/Get_Data/data.csv')
+df.to_csv('/home/code/NLP/LSTM/First_model_of_mine/Get_Data/data2.csv')
